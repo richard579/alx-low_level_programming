632eec176a8f3e792b7a65c38bcc3a57c71a908d
richard579 <richardbeng358@outlook.com>
2023-02-21 11:11:38 -0800

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,17 +8,14 @@
 int main(void)
 {
 	int i, sum = 0;
-
-
+	
 	for (i = 0; i < 1024; i++)
 	{
 		if ((i % 3) == 0 || (i % 5) == 0)
 			sum += i;
 	}
-
-
+	
 	printf("%d\n", sum);
-
-
+	
 	return (0);
 }
