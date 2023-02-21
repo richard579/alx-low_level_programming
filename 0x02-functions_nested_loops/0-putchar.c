828b451d8ed2b12c5902d940ef5016648d8a2d7d
richard579 <richardbeng358@outlook.com>
2023-02-21 03:33:09 -0800

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * main - Entry point
+ * _putchar - print _putchar
  *
- * Return : Always 0 (Success)
+ * Return: 0
  */
 
-int main(void)
+int _putchar(void)
 {
 	_putchar('_');
 	_putchar('p');
