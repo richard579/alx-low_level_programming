062f4cca5bfe96782842f3bdc51a60c416923853
richard579 <richardbeng358@outlook.com>
2023-02-23 04:55:33 -0800

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
-#include <math.h>
+
 /**
  * main - finds and prints the largest prime factor of a number
+ *
  * Return: Always 0
  */
-
 int main(void)
 {
 	unsigned long int num = 612852475143;
