ed4af7be89a510bac1cfd6445730c80124f38e92
richard579 <richardbeng358@outlook.com>
2023-04-10 09:42:57 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,26 +11,24 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int i = 0, file;
+	int fd, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	if (text_content == NULL)
-		text_content = "";
-
-	while (text_content[i] != '\0')
+	if (text_content != NULL)
 	{
-		i++;
+		for (len = 0; text_content[len];)
+			len++;
 	}
 
-	file = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	w = write(fd, text_content, len);
 
-	if (file == -1)
+	if (fd == -1 ||  w == -1)
 		return (-1);
 
-	write(file, text_content, i);
+	close(fd);
 
 	return (1);
 }
